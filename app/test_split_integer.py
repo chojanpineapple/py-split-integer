32d3a1f9e376d070924cd1b5ee8fe8fb6f540eec
chojanpineapple <johan.apr99@gmail.com>
2026-01-13 13:43:01 +0000

    test

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -6,7 +6,7 @@ def test_sum_of_the_parts_should_be_equal_to_value() -> None:
     parts = 4
     result = split_integer(value, parts)
 
-    assert sum(result) == value; len(result) == parts
+    assert sum(result) == value and len(result) == parts
 
 
 def test_should_split_into_equal_parts_when_value_divisible_by_parts() -> None:
@@ -26,7 +26,7 @@ def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
     parts = 6
     result = split_integer(value, parts)
 
-    assert result == sorted(result); assert max(result) - min(result) <= 1
+    assert result == sorted(result) and assert max(result) - min(result) <= 1
 
 
 def test_should_add_zeros_when_value_is_less_than_number_of_parts() -> None:
